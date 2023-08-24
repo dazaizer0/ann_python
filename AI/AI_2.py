cdac196193ab38aa48cf7e1f8c0f26dd3f27f423
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-08-24 11:53:25 +0200

    1n

--- AI/AI_2.py
+++ AI/AI_2.py
@@ -1,9 +1,12 @@
 # zamien is positive na czy jest poprawne i jakie powu=inno byc a nie jakie jest
+file = open("aidata.js", 'a')
+
 class Neuron:
-    def __init__(self, x, w, answer: bool):
+    def __init__(self, x, w, answer: bool, nreturn: bool):
         self.x = x
         self.w = w
         self.answer = answer
+        self.nreturn = nreturn
 
 
 def ACTIVATE(n: Neuron,
@@ -35,29 +38,54 @@ def TEACH(n: Neuron):
     print(n.x)
     print(n.w)
 
+def LOG(x, w, b, a, nret, answ):
+    file.write(f'FIRST >> W: {w}\n')
+    file.write(f'BEFORE >> W: {w}, B: {b}, IS_POSITIVE: {answ}\n')
+    file.write(f'AFTER >> W: {w}, B: {b}, IS_POSITIVE: {answ}\n')
+    file.write(f'>> {a}\n')
+    file.write('\n')
 
-D = 0.2
-B = 6
+
+D = 0.01
+B = 4
 A = 0
 
-neuron1 = Neuron([23, 4], [-2, 0.1], True)
-for i in range(1, 50):
+neuron1 = Neuron([18, 34], [2, 3], False, not False)
+
+i = 1
+while neuron1.nreturn != neuron1.answer:
     if i == 1:
         A = ACTIVATE(neuron1, B)
 
         print(f'FIRST >> W: {neuron1.w}, B: {B}')
         print(f'{i} >> {A}')
 
-        if A > 0:
-            print("O+")
+        if neuron1.answer == True:
+            if A > 0:
+                neuron1.nreturn = True
+            elif A < 0:
+                neuron1.nreturn = False
         else:
-            print("D-")
+            if A < 0:
+                neuron1.nreturn = False
+            elif A < 0:
+                neuron1.nreturn = True
 
+        if neuron1.answer == neuron1.nreturn:
+            if neuron1.answer == True:
+                print("Obraczka")
+                file.write('obraczka\n')
+            else:
+                print("Dlugopis")
+                file.write('dlugopis\n')
+        else:
+            print("...")
+
+        LOG(neuron1.x, neuron1.w, B, A, neuron1.nreturn, neuron1.answer)
         print()
     else:
         is_positive = neuron1.answer
 
-        # ACTIONS
         print(f'BEFORE >> W: {neuron1.w}, B: {B}, IS_POSITIVE: {is_positive}')
 
         neuron1.w = RETURN_NEW_W(D, neuron1.w, neuron1.x, is_positive)
@@ -67,9 +95,31 @@ for i in range(1, 50):
         A = ACTIVATE(neuron1, B)
         print(f'{i} >> {A}')
 
-        if A > 0:
-            print("O+")
+        if neuron1.answer == True:
+            if A > 0:
+                neuron1.nreturn = True
+            elif A < 0:
+                neuron1.nreturn = False
         else:
-            print("D-")
+            if A < 0:
+                neuron1.nreturn = False
+            elif A < 0:
+                neuron1.nreturn = True
 
+        if neuron1.answer == neuron1.nreturn:
+            if neuron1.answer == True:
+                print("Obraczka")
+                file.write('obraczka\n')
+            else:
+                print("Dlugopis")
+                file.write('dlugopis\n')
+        else:
+            print("...")
+
+        LOG(neuron1.x, neuron1.w, B, A, neuron1.nreturn, neuron1.answer)
         print()
+    i += 1
+
+
+file.write(f'{A} | {neuron1.w} | {B} | {neuron1.answer}\n')
+file.close()
