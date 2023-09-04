1752fc486150d2814ce990db4e966f70f72053b4
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-09-04 20:31:50 +0200

    WEBVR

--- NeogusLLNeuronNetwork/test.py
+++ NeogusLLNeuronNetwork/test.py
@@ -1,2 +1,7 @@
-x = 10
-print(x)
+# Rozdziel ciąg znaków na podstawie przecinka i przekształć w liczby całkowite
+ciag_znakow = "5, 10"
+elementy = ciag_znakow.split(",")  # Rozdziel na podstawie przecinka
+lista = [int(element) for element in elementy]
+
+# Wyświetl wynik
+print(lista)
