03bc6a55436aa7c8d1c8f9df372f8e7943b910da
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-09-04 23:02:30 +0200

    next_gen

--- NeogusLLNeuronNetwork/test.py
+++
@@ -1,7 +0,0 @@
-# Rozdziel ciąg znaków na podstawie przecinka i przekształć w liczby całkowite
-ciag_znakow = "5, 10"
-elementy = ciag_znakow.split(",")  # Rozdziel na podstawie przecinka
-lista = [int(element) for element in elementy]
-
-# Wyświetl wynik
-print(lista)
