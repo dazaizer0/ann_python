648bd7846c3f1312a280563eab6bb4597ec66524
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-09-10 15:11:30 +0200

    wtf

--- ANN-ArtificialNeuralNetworks/test/test.py
+++ ANN-ArtificialNeuralNetworks/test/test.py
@@ -1,4 +1,54 @@
-import AILib as a
+class var:
+    def __init__(self, var_type, value):
+        self.var_type = var_type
+        self.value = value
 
-n = a.Neuron([2, 5, 13, 3], [-4, 0.5, -2, 4], True, False)
-print(n.ACTIVATE(6))
+    def __repr__(self):
+        return str(self.value)
+
+    def __sub__(self, other):
+        return self.value + other
+
+
+vars = [var(int, 0), ]
+
+file = open('test.txt', 'r')
+data = file.read()
+data = data.split(';')
+
+for i in data:
+    i.replace('\n', '')
+
+for i in range(len(data)):
+    if data[i] == "cvar":
+        temp_var = var(str(data[i + 1]), data[i + 2])
+        vars.append(temp_var)
+
+    if data[i] == "out":
+        if data[i + 1] == "var":
+            nr = int(data[i + 2])
+            print(vars[nr])
+        if data[i + 1] == "sum":
+            if data[i + 2] == "var":
+                what = data[i + 2]
+                sum = 0
+                for l in range(len(what)):
+                    var_list = []
+                    try:
+                        var_list.append(int(vars[l].value))
+                    except:
+                        continue
+                    for nr in var_list:
+                        sum += nr
+                print(sum)
+            else:
+                what = data[i + 2]
+                sum = 0
+                for l in what:
+                    try:
+                        l = int(l)
+                    except:
+                        continue
+                    if i != "+":
+                        sum += l
+                print(sum)
