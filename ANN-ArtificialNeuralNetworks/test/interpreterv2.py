dcefe9d6c8373c05eda8d69b603667ebca43becd
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-09-19 18:55:52 +0200

    .

--- ANN-ArtificialNeuralNetworks/test/interpreterv2.py
+++ ANN-ArtificialNeuralNetworks/test/interpreterv2.py
@@ -5,16 +5,42 @@ data = data.replace('\n', '')
 data = data.replace(';', ' ;')
 data = data.split(' ')
 
+class variable:
+    def __init__(self, typ, var):
+        self.typ = typ
+
+        if typ == "int":
+            self.var = int(var)
+        if typ == "str":
+            self.var = str(var)
+        else:
+            self.var = var
+
+    def __repr__(self):
+        return str(self.var)
+
+
+variables = {
+
+}
+
 for i in range(len(data)):
+    if data[i] == "cre" and data[i + 4]:
+        temp: variable = variable(data[i + 1], data[i + 3])
+        variables[data[i + 2]] = temp
+
     if data[i] == "out":
-        j = i + 1
-        output = ""
-        while data[j] != ";":
-            try:
-                temp: int = int(data[j])
-                char = chr(temp)
-                output += char
-            except:
-                output += "-"
-            j += 1
-        print(output)
+        if data[i + 1] in variables.keys():
+            print(variables[data[i + 1]])
+        else:
+            j = i + 1
+            output = ""
+            while data[j] != ";":
+                try:
+                    temp: int = int(data[j])
+                    char = chr(temp)
+                    output += char
+                except:
+                    output += "-"
+                j += 1
+            print(output)
