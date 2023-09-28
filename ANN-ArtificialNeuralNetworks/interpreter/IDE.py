5d29e98b6f4a7101c614cd358ea491a8d2ca78e9
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-09-28 23:14:34 +0200

    IDE

--- ANN-ArtificialNeuralNetworks/interpreter/IDE.py
+++ ANN-ArtificialNeuralNetworks/interpreter/IDE.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import scrolledtext
+from tkinter import filedialog
 import subprocess
 import interpreterv2 as ann
+import webbrowser
 
 
 def run_code():
@@ -14,34 +16,52 @@ def run_code():
     code_output.insert(tk.END, str(result))
     code_output.config(state="disabled")
 
-def save_code():
-    code = code_input.get(1.0, tk.END)
-    try:
-        with open("temp_script.ann", "w") as temp_file:
-            temp_file.write(code)
-    except Exception as e:
-        code_input.config(state="normal")
-        code_input.delete(1.0, tk.END)
-        code_input.insert(tk.END, str(e))
-        code_input.config(state="disabled")
+def save_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt")])
+    if file_path:
+        with open(file_path, "w") as file:
+            text = code_input.get(1.0, tk.END)
+            file.write(text)
+
+def load_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
+    if file_path:
+        with open(file_path, "r") as file:
+            text = file.read()
+            code_input.delete(1.0, tk.END)
+            code_input.insert(tk.END, text)
+
+def open_documentation():
+    url = "https://github.com/dazaizer0/Neogus-ANN/releases"
+    webbrowser.open(url)
 
 
 root = tk.Tk()
 root.title("ANN IDE")
+root.configure(bg="grey4")
+root.iconbitmap(
+    r'C:\Users\mydat\Documents\_python\NeogusAI_Github\NeogusAI\ANN-ArtificialNeuralNetworks\interpreter\annidelogo.png'
+)
 
 # INPUT
-code_input = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=60, height=20)
+code_input = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=60, height=20, bg="grey8", fg="plum2")
 code_input.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
 # BUTTONS
-run_button = tk.Button(root, text="run code", command=run_code)
+run_button = tk.Button(root, text="run code", command=run_code, bg="grey8", fg="plum2")
 run_button.pack(side=tk.LEFT, padx=10, pady=10)
 
-run_button = tk.Button(root, text="save code", command=save_code)
-run_button.pack(side=tk.LEFT, padx=10, pady=10)
+save_button = tk.Button(root, text="save code", command=save_file, bg="grey8", fg="plum2")
+save_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+load_button = tk.Button(root, text="load code", command=load_file, bg="grey8", fg="plum2")
+load_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+docu_button = tk.Button(root, text="open_documentation", command=open_documentation, bg="grey8", fg="plum2")
+docu_button.pack(side=tk.LEFT, padx=10, pady=10)
 
 # OUTPUT
-code_output = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=60, height=10)
+code_output = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=60, height=10, bg="grey8", fg="magenta2")
 code_output.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
 
 root.mainloop()
